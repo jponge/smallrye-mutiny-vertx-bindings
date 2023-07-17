a888d02595bfe87174d9b1b186d778216c64a5c3
Julien Ponge <jponge@redhat.com>
2023-07-17 18:10:54 +0200

    WIP

--- vertx-mutiny-clients/vertx-mutiny-core/src/main/java/io/smallrye/mutiny/vertx/core/ContextAwareScheduler.java
+++ vertx-mutiny-clients/vertx-mutiny-core/src/main/java/io/smallrye/mutiny/vertx/core/ContextAwareScheduler.java
@@ -2,13 +2,9 @@ package io.smallrye.mutiny.vertx.core;
 
 import static java.util.Objects.requireNonNull;
 
-import java.util.Collection;
-import java.util.List;
 import java.util.concurrent.*;
-import java.util.function.Supplier;
 
 import io.smallrye.common.annotation.CheckReturnValue;
-import io.vertx.core.impl.ContextInternal;
 import io.vertx.mutiny.core.Context;
 import io.vertx.mutiny.core.Vertx;
 
@@ -25,8 +21,6 @@ import io.vertx.mutiny.core.Vertx;
  * These executors cannot be shut down.
  */
 public interface ContextAwareScheduler {
-
-    // ---- "DSL" ---- //
 
     /**
      * Define a scheduler {@link ScheduledExecutorService} delegate.
@@ -59,7 +53,7 @@ public interface ContextAwareScheduler {
          */
         public ScheduledExecutorService withContext(Context context) {
             requireNonNull(context, "The context cannot be null");
-            return new InternalExecutor(delegate, () -> context);
+            return new ContextAwareExecutorWrapper(delegate, () -> context);
         }
 
         /**
@@ -71,7 +65,7 @@ public interface ContextAwareScheduler {
          */
         public ScheduledExecutorService withGetOrCreateContext(Vertx vertx) {
             requireNonNull(vertx, "The Vertx object cannot be null");
-            return new InternalExecutor(delegate, vertx::getOrCreateContext);
+            return new ContextAwareExecutorWrapper(delegate, vertx::getOrCreateContext);
         }
 
         /**
@@ -104,122 +98,6 @@ public interface ContextAwareScheduler {
                 throw new IllegalStateException("There is no Vert.x context in the current thread: " + Thread.currentThread());
             }
             return context;
-        }
-    }
-
-    // ---- Executor class ---- //
-
-    class InternalExecutor implements ScheduledExecutorService {
-
-        private final ScheduledExecutorService delegate;
-
-        private final Supplier<Context> contextSupplier;
-
-        private InternalExecutor(ScheduledExecutorService delegate, Supplier<Context> contextSupplier) {
-            this.delegate = delegate;
-            this.contextSupplier = contextSupplier;
-        }
-
-        private Runnable decorate(Runnable task) {
-            return () -> {
-                Context context = contextSupplier.get();
-                ContextInternal contextInternal = (ContextInternal) context.getDelegate();
-                if (!contextInternal.isDuplicate()) {
-                    context = Context.newInstance(contextInternal.duplicate());
-                }
-                context.runOnContext(task);
-            };
-        }
-
-        // ---- ScheduledExecutorService ---- //
-
-        @Override
-        public ScheduledFuture<?> schedule(Runnable command, long delay, TimeUnit unit) {
-            return delegate.schedule(decorate(command), delay, unit);
-        }
-
-        @Override
-        public <V> ScheduledFuture<V> schedule(Callable<V> callable, long delay, TimeUnit unit) {
-            throw new UnsupportedOperationException();
-        }
-
-        @Override
-        public ScheduledFuture<?> scheduleAtFixedRate(Runnable command, long initialDelay, long period, TimeUnit unit) {
-            return delegate.scheduleAtFixedRate(decorate(command), initialDelay, period, unit);
-        }
-
-        @Override
-        public ScheduledFuture<?> scheduleWithFixedDelay(Runnable command, long initialDelay, long delay, TimeUnit unit) {
-            return delegate.scheduleWithFixedDelay(decorate(command), initialDelay, delay, unit);
-        }
-
-        // ---- ExecutorService ---- //
-
-        @Override
-        public void shutdown() {
-            throw new UnsupportedOperationException();
-        }
-
-        @Override
-        public List<Runnable> shutdownNow() {
-            throw new UnsupportedOperationException();
-        }
-
-        @Override
-        public boolean isShutdown() {
-            return false;
-        }
-
-        @Override
-        public boolean isTerminated() {
-            return false;
-        }
-
-        @Override
-        public boolean awaitTermination(long timeout, TimeUnit unit) {
-            throw new UnsupportedOperationException();
-        }
-
-        @Override
-        public <T> Future<T> submit(Callable<T> task) {
-            throw new UnsupportedOperationException();
-        }
-
-        @Override
-        public <T> Future<T> submit(Runnable task, T result) {
-            return delegate.submit(decorate(task), result);
-        }
-
-        @Override
-        public Future<?> submit(Runnable task) {
-            return delegate.submit(decorate(task));
-        }
-
-        @Override
-        public <T> List<Future<T>> invokeAll(Collection<? extends Callable<T>> tasks) {
-            throw new UnsupportedOperationException();
-        }
-
-        @Override
-        public <T> List<Future<T>> invokeAll(Collection<? extends Callable<T>> tasks, long timeout, TimeUnit unit) {
-            throw new UnsupportedOperationException();
-        }
-
-        @Override
-        public <T> T invokeAny(Collection<? extends Callable<T>> tasks) {
-            throw new UnsupportedOperationException();
-        }
-
-        @Override
-        public <T> T invokeAny(Collection<? extends Callable<T>> tasks, long timeout, TimeUnit unit) {
-            throw new UnsupportedOperationException();
-        }
-
-        // ---- Executor ---- //
-
-        @Override
-        public void execute(Runnable command) {
-            delegate.execute(decorate(command));
         }
     }
 }
